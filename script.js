b251b760f923c198bb83547da33c385c3ded0e4c
ZarshadHussain <zarshadhussain719@gmail.com>
2024-07-09 02:45:09 -0700

    This is 10 no assingment of js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Smallest Number program....
+
+// var numbers = [34, 15, 88, 2, 23];
+
+// var smallest = numbers[0];
+
+// for (var i = 1; i < numbers.length; i++) {
+//     if (numbers[i] < smallest) {
+//         smallest = numbers[i];
+//     }
+// }
+
+// console.log(smallest); 
+
+
+// Faharenheit program.....
+
+// function celsiusToFahrenheit(celsius) {
+//     return (celsius * 9/5) + 32;
+// }
+
+// // Example usage:
+// var celsius = 25;
+// var fahrenheit = celsiusToFahrenheit(celsius);
+// console.log(fahrenheit);
+
+// Without Function
+
+// const celsius = 25;
+// const fahrenheit = (celsius * 9/5) + 32;
+
+// console.log(fahrenheit);
